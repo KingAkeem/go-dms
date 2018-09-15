7b85723e52231a33acd17591ecc213d3df0a7441
Akeem King <akeemtlking@gmail.com>
2018-09-14 21:48:18 -0400

    Removing testing example and main package

--- dms.go
+++ dms.go
@@ -1,4 +1,4 @@
-package main 
+package dms 
 
 import (
         "fmt"
@@ -25,9 +25,11 @@ func newDMS(lat, lon float64) (*DMS, *DMS, error) {
     if lat > 90 || lon > 180 {
         return nil, nil, &LatLonError{"Latitude must be less than 90 and longitude must be less than 180."}
     }
+
     latitude := uint8(lat)
     latitudeMinutes := uint8((lat - float64(latitude)) * 60)
     latitudeSeconds := uint8((lat - float64(latitude) - float64(latitudeMinutes) / 60) * 3600)
+
     longitude := uint8(lon)
     longitudeMinutes := uint8((lon - float64(longitude)) * 60)
     longitudeSeconds := uint8((lon - float64(longitude) - float64(longitudeMinutes) / 60) * 3600)
@@ -45,14 +47,4 @@ func newDMS(lat, lon float64) (*DMS, *DMS, error) {
     }
 
     return latDms, lonDms, nil
-
-}
-
-func main() {
-    latDms, lonDms, err := newDMS(52.45, 12.02)
-    if err != nil {
-        panic(err)
-    }
-    fmt.Println("Latitude: 45.90\nLongitude: 12.02")
-    fmt.Printf("Latitude DMS: %+v\nLongitude DMS: %+v\n", latDms, lonDms)
 }
