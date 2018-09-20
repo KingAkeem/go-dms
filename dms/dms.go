78ce6d0eaae451a8087c893be39d92e0854136d9
Akeem King <akeemtlking@gmail.com>
2018-09-19 20:24:13 -0400

    Adding formatting and hemisphere to coordinates

--- dms/dms.go
+++ dms/dms.go
@@ -1,5 +1,9 @@
 package dms 
 
+import (
+    "fmt"
+)
+
 type LatLonError struct {
     err string
 }
@@ -12,6 +16,11 @@ type DMS struct {
     degrees uint8
     minutes uint8
     seconds float64 
+    direction string 
+}
+
+func (d *DMS) String() string {
+    return fmt.Sprintf(`%d° %d' %f" %s`, d.degrees, d.minutes, d.seconds, d.direction)
 }
 
 func NewDMS(lat, lon float64) (*DMS, *DMS, error) {
@@ -22,6 +31,20 @@ func NewDMS(lat, lon float64) (*DMS, *DMS, error) {
         return nil, nil, &LatLonError{"Latitude must be less than 90 and longitude must be less than 180."}
     }
 
+    var latDirection string 
+    var lonDirection string 
+    if lat > 0 {
+        latDirection = "N"
+    } else {
+        latDirection = "S"
+    }
+
+    if lon > 0 {
+        lonDirection = "E"
+    } else {
+        lonDirection = "W"
+    }
+
     latitude := uint8(lat)
     latitudeMinutes := uint8((lat - float64(latitude)) * 60)
     latitudeSeconds := (lat - float64(latitude) - float64(latitudeMinutes) / 60) * 3600
@@ -30,7 +53,7 @@ func NewDMS(lat, lon float64) (*DMS, *DMS, error) {
     longitudeMinutes := uint8((lon - float64(longitude)) * 60)
     longitudeSeconds := (lon - float64(longitude) - float64(longitudeMinutes) / 60) * 3600
 
-    return &DMS{degrees: latitude, minutes: latitudeMinutes, seconds: latitudeSeconds,}, 
-           &DMS{degrees: longitude, minutes: longitudeMinutes, seconds: longitudeSeconds,}, 
+    return &DMS{degrees: latitude, minutes: latitudeMinutes, seconds: latitudeSeconds, direction: latDirection}, 
+    &DMS{degrees: longitude, minutes: longitudeMinutes, seconds: longitudeSeconds, direction: lonDirection}, 
            nil
 }
